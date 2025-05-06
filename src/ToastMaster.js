2de6c9cda3778cd77faccb3675f0dd436963af6e
sabbirsam <61382787+sabbirsam@users.noreply.github.com>
2025-05-06 18:10:58 +0600

    fixed issues to use in js and has initilize issues

--- src/ToastMaster.js
+++ src/ToastMaster.js
@@ -9,7 +9,8 @@
 (function(global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
-    (global = global || self, global.Toast = factory());
+    // (global = global || self, global.Toast = factory());
+    (global = global || self, (global.Toast = global.ToastMaster = factory()));
   })(this, function() {
     'use strict';
   
